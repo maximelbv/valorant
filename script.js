f11e5318d5c15994e5c6735d8a8d1598d6b029ed
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2022-10-05 15:55:15 +0200

    add thumbnail swiper & display agents icon into them

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+// let swiper = new Swiper(".mySwiper", {
+//     observer: true,
+//     observerParents: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         renderBullet: function (index, className) {
+//         return '<span class="' + className + '">' + (index + 1) + "</span>";
+//         },
+//     },
+// });
+
 var swiper = new Swiper(".mySwiper", {
     observer: true,
     observerParents: true,
+    spaceBetween: 10,
+    slidesPerView: 20,
+    freeMode: true,
+    watchSlidesProgress: true,
+    });
+    var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
     },
 });
 
@@ -15,8 +37,17 @@ $(function(){
     })
 
     .done(function(res){
+
         for (let i=0; i<= res.data.length; i++) {
-            console.log(res.data[i].displayName)
+
+            let name = res.data[i].displayName;
+            let icon = res.data[i].displayIcon;
+            let desc = res.data[i].description;
+            let illustration = res.data[i].fullPortrait;
+            let background = res.data[i].background;
+            let bgColor = res.data[i].backgroundGradientColors[0];
+            
+
             $('.swiper-wrapper').append(`
                 <div class="swiper-slide">
                     <p class='name'>${res.data[i].displayName}</p>
